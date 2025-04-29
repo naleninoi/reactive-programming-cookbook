37010623df946857729a1d7f81ba07cf59f318bf
viktor.kochetov-arkhipov <viktor.kochetov-arkhipov@sms-a.ru>
2025-04-29 13:23:46 +0300

    recipe list, recipe item, fix images

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,9 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
+import { RecipesListComponent } from './components/recipes-list/recipes-list.component';
+import { Recipe } from './types/recipes.type';
+import { RecipesService } from './services/recipes.service';
 
 @Component({
   selector: 'app-root',
@@ -14,10 +17,17 @@ import { MatInputModule } from '@angular/material/input';
     MatButtonModule,
     MatIconModule,
     ReactiveFormsModule,
+    RecipesListComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'Reactive-Programming-Cookbook';
+
+  recipes: Recipe[] = [];
+
+  constructor(private recipesService: RecipesService) {
+    this.recipesService.getRecipes$().subscribe(recipes => this.recipes = recipes);
+  }
 }
